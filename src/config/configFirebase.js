1858553227985207b1bbeb02b0b1dc6db1aa0d4a
AngelesCahill <70714713+AngelesCahill@users.noreply.github.com>
2021-03-26 15:17:24 -0300

    primer commit

---
+++ src/config/configFirebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCjfbvjBzczVQSbpC7-3kYzSUl2cRxQpFI",
+    authDomain: "examen-final-52705.firebaseapp.com",
+    projectId: "examen-final-52705",
+    storageBucket: "examen-final-52705.appspot.com",
+    messagingSenderId: "549256687748",
+    appId: "1:549256687748:web:ee8e8e5431903231b7d2d1"
+  };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import { firebaseConfig } from './config/configFirebase.js';
+import firebase from 'firebase';
 
+firebase.initializeApp(firebaseConfig);
 Vue.config.productionTip = false
 
 new Vue({
